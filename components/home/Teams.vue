c0b234a31101bd770ac492a3a73abef96791ab6d
msbarf14 <misbahularifin14@gmail.com>
2023-02-18 02:28:09 +0800

    update team

--- components/home/Teams.vue
+++ components/home/Teams.vue
@@ -1,3 +1,33 @@
+<script setup>
+import { ref } from "vue"
+import { UserIcon } from "@heroicons/vue/24/outline"
+const teams = ref([
+    {
+        name: 'Mario Lee A',
+        position: 'Owner'
+    },
+    {
+        name: 'Intan Rachma A',
+        position: 'CEO'
+    },
+    {
+        name: 'Haris Nata H',
+        position: 'General Manager'
+    },
+    {
+        name: 'Maya Agnesia P & Alpie Sarah',
+        position: 'Sales'
+    },
+    {
+        name: 'Rerevina, Alvita, kiky Saputri',
+        position: 'Creative'
+    },
+    {
+        name: 'Youn Thalya Masoko, Heri Mulyono, Rizky',
+        position: 'Designer'
+    }
+])
+</script>
 <template>
     <div >
         <div class="mx-auto max-w-7xl py-12 text-center lg:py-24">
@@ -8,42 +38,16 @@
                         lectus sit pretium egestas vel mattis neque.</p>
                 </div>
                 <ul role="list"
-                    class="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:max-w-5xl lg:grid-cols-3">
-                    <li>
+                    class="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:max-w-5xl lg:grid-cols-6">
+                    <li v-for="item in teams">
                         <div class="space-y-6 relative">
-                            <img class="mx-auto h-40 w-40 object-cover rounded-2xl xl:h-56 xl:w-56"
-                                src="https://images.unsplash.com/photo-1543060829-a0029874b174?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                                alt="">
-                            <div class="space-y-2">
-                                <div class="space-y-1 text-lg font-medium leading-6">
-                                    <h3 class="text-white">MARIO LEE</h3>
-                                    <p class="text-brand-cream">Founder</p>
-                                </div>
+                            <div class="w-40 h-40 mx-auto bg-brand-cream flex flex-col justify-center items-center rounded-lg">
+                                <UserIcon class="w-24 place-items-center text-slate-5"/>
                             </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="space-y-6">
-                            <img class="mx-auto h-40 w-40 object-cover object-center rounded-2xl xl:h-56 xl:w-56"
-                                src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80"
-                                alt="">
                             <div class="space-y-2">
                                 <div class="space-y-1 text-lg font-medium leading-6">
-                                    <h3 class="text-white">INTAN RACHMA AULITA</h3>
-                                    <p class="text-brand-cream">Founder</p>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="space-y-6">
-                            <img class="mx-auto h-40 w-40 rounded-2xl xl:h-56 xl:w-56"
-                                src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
-                                alt="">
-                            <div class="space-y-2">
-                                <div class="space-y-1 text-lg font-medium leading-6">
-                                    <h3 class="text-white">Whitney Francis</h3>
-                                    <p class="text-brand-cream">Copywriter</p>
+                                    <h3 class="text-white">{{item.name}}</h3>
+                                    <p class="text-brand-cream">{{item.position}}</p>
                                 </div>
                             </div>
                         </div>
